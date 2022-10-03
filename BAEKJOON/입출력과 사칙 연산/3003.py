cdc6b16f9a6ea04ffc15642caac1ce23e6610308
Baek Gi Chan <laphayen@gmail.com>
2022-10-03 15:46:51 +0900

    Upload 3003.py

---
+++ BAEKJOON/입출력과 사칙 연산/3003.py
@@ -0,0 +1,9 @@
+k0, q0, l0, b0, n0, p0 = 1, 1, 2, 2, 2, 8
+k1, q1, l1, b1, n1, p1 = input().split()
+k1 = int(k1)
+q1 = int(q1)
+l1 = int(l1)
+b1 = int(b1)
+n1 = int(n1)
+p1 = int(p1)
+print(k0-k1, q0-q1, l0-l1, b0-b1, n0-n1, p0-p1)
