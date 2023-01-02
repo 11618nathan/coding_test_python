5f3eb033579b1c590d4d0a6f5486038d67519f58
Baek Gi Chan <laphayen@gmail.com>
2023-01-02 23:43:19 +0900

    Upload 5717.py

---
+++ BAEKJOON/etc/5717.py
@@ -0,0 +1,7 @@
+A, B = map(int, input().split())
+print(B-A, B)a, b = map(int, input().split())
+while 1:
+    if (a==0) and (b==0):
+        break
+    print(a+b)
+    a, b = map(int, input().split())
