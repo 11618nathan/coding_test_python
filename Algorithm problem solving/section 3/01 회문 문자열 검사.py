9c241a79043539a83b9f258adc994abb6b648511
Baek Gi Chan <laphayen@gmail.com>
2023-02-17 14:40:21 +0900

    Upload 01 회문 문자열 검사.py

---
+++ Algorithm problem solving/section 3/01 회문 문자열 검사.py
@@ -0,0 +1,16 @@
+N = int(input())
+result = 'Yes'
+for i in range(N):
+    s = input()
+    s = s.upper()
+    size = len(s)
+    for j in range(size//2):
+        if s[j] != s[-1-j]:
+            print('#%d NO' %(i+1))
+            break
+    else:
+        print('#%d YES' %(i+1))  
+    if s == s[::-1]:
+        print('#%d YES' %(i+1))
+    else:
+        print('#%d NO' %(i+1))
