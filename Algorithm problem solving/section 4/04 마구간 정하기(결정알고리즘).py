c85ca3dc0489eb41d59247f8957971102ecefa4d
Baek Gi Chan <laphayen@gmail.com>
2023-05-04 23:18:11 +0900

    Upload 04 마구간 정하기(결정알고리즘).py

---
+++ Algorithm problem solving/section 4/04 마구간 정하기(결정알고리즘).py
@@ -0,0 +1,25 @@
+def Count(len):
+    cnt = 1
+    ep = line[0]
+    for i in range(1, n):
+        if line[i] - ep >= len:
+            cnt += 1
+            ep = line[i]
+    return cnt
+
+n, c = map(int, input().split())
+line = []
+for _ in range(n):
+    tmp = int(input())
+    line.append(tmp)
+line.sort()
+lt = 1
+rt = line[n-1]
+while lt < rt:
+    mid = (lt+rt) // 2
+    if Count(mid) >= c:
+        res = mid
+        lt = mid + 1
+    else:
+        rt = mid - 1
+print(res)
